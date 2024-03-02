9dd3953cc3576812f5ce65ad1ea7fe8c7f7f71b2
MHannan <muhammadhannan2001@gmail.com>
2024-03-02 21:46:06 +0500

    DOM

---
+++ Dom/script.js
@@ -0,0 +1,46 @@
+// // 
+// let elements = document.querySelector('.heading');
+// console.dir(elements)
+
+// document.querySelector("div").children;
+
+// let string = document.querySelector('h2');
+// string.append(' from this side');
+// // console.dir(string.innerText)
+// // string.innerText = string.innerText + ' from this side';
+
+// let divs = document.querySelectorAll('.box');
+// console.dir(divs)
+// for(let i = 0; i < divs.length; i++)
+//     divs[i].innerText = 'Hello javascript';
+
+// let div = document.querySelector('div');
+// console.log(div);
+// let id = div.getAttribute('id');
+// console.log(id)
+
+// div.setAttribute('id', 'boxes');
+// div.style.backgroundColor = "green"
+// //div.style.visibility = 'hidden'
+
+// let btn = document.createElement('button');
+// btn.innerText = 'Click me!';
+
+// let div = document.querySelector('h2');
+// div.append(btn)
+// div.prepend(btn)
+// div.before(btn)
+// div.after(btn)
+
+// btn.remove()
+
+// let btn = document.createElement('button');
+// btn.innerText = 'Click Me!';
+// btn.style.backgroundColor = 'red';
+// btn.style.color = 'white';
+
+// let body = document.querySelector('body');
+// body.prepend(btn);
+
+let p = document.querySelector('p');
+p.classList.add('paras')
